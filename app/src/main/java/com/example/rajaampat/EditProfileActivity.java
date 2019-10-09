849bfb836dc2fc99bf15cd5691c0ab23581fc77d
yazidm.f <yazidmf4869@gmail.com>
2019-10-09 16:56:19 +0700

    Mukhlash yang push

--- app/src/main/java/com/example/rajaampat/EditProfileActivity.java
+++ app/src/main/java/com/example/rajaampat/EditProfileActivity.java
@@ -2,13 +2,35 @@ package com.example.rajaampat;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class EditProfileActivity extends AppCompatActivity {
+
+    // Tombol Balik
+    ImageView balik;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_profile);
+
+        // Buat Fungsi Tombol Kembali Di Toolbar
+        balik = findViewById(R.id.kembali_editprofile);
+        balik.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        Intent backToProfile = new Intent(EditProfileActivity.this, HomeActivity.class);
+        startActivity(backToProfile);
+        finish();
     }
 }
