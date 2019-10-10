41b8d0309ee3353c4de50d6e45bca81766655d2f
Abdullah Mukhlashin <abdullahmukhlashiin@gmail.com>
2019-10-10 23:36:00 +0700

    Adding the Destination and Hotel Section

--- app/src/main/java/com/example/rajaampat/HomeActivity.java
+++ app/src/main/java/com/example/rajaampat/HomeActivity.java
@@ -6,13 +6,27 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 
 public class HomeActivity extends AppCompatActivity {
+
+    RelativeLayout rlvHotel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        rlvHotel = findViewById(R.id.rlv_hotel);
+
+        rlvHotel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent goToHotel = new Intent(HomeActivity.this, HotelActivity.class);
+                startActivity(goToHotel);
+            }
+        });
+
     }
 }
 
