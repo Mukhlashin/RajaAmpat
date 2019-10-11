94e65c62f45ce34aeea51d5c95fd261aa0d29fc1
Abdullah Mukhlashin <abdullahmukhlashiin@gmail.com>
2019-10-11 15:59:11 +0700

    Adding Contact Page + Trying to Building the Fragment Transition Section

---
+++ app/src/main/java/com/example/rajaampat/SplashFragment2.java
@@ -0,0 +1,32 @@
+package com.example.rajaampat;
+
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class SplashFragment2 extends Fragment {
+
+
+    public SplashFragment2() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_splash2, container, false);
+
+    }
+
+}
--- app/src/main/java/com/example/rajaampat/SplashScreen.java
+++ app/src/main/java/com/example/rajaampat/SplashScreen.java
@@ -1,14 +1,29 @@
 package com.example.rajaampat;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.WindowManager;
+import android.view.animation.AnimationUtils;
 
 public class SplashScreen extends AppCompatActivity {
+
+    FragmentManager fm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        fm = getSupportFragmentManager();
+        fm.beginTransaction()
+                .add(R.id.fragment_container, new SplashFragment())
+                .commit();
+
     }
 }
