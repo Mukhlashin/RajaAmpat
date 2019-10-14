3fcf1ec1b4b57d2ba5bcea887bee9c10a002eabc
Abdullah Mukhlashin <abdullahmukhlashiin@gmail.com>
2019-10-14 14:18:09 +0700

    Fixin the Main Page

---
+++ app/src/main/java/com/example/rajaampat/DetailReportActivity.java
@@ -0,0 +1,14 @@
+package com.example.rajaampat;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DetailReportActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_report);
+    }
+}
