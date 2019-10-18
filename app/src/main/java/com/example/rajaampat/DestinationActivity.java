e623c461b421c2f0a1b72eb2f2381d1b63b9c699
yazidm.f <yazidmf4869@gmail.com>
2019-10-18 11:24:53 +0700

    Buat Dummy V2

--- app/src/main/java/com/example/rajaampat/DestinationActivity.java
+++ app/src/main/java/com/example/rajaampat/DestinationActivity.java
@@ -6,10 +6,13 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.LinearLayout;
 
 public class DestinationActivity extends AppCompatActivity {
 
     ImageButton imbBack;
+
+    LinearLayout btnDetail;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +20,15 @@ public class DestinationActivity extends AppCompatActivity {
         setContentView(R.layout.activity_destination);
 
         imbBack = findViewById(R.id.imb_back);
+        btnDetail = findViewById(R.id.btn_detail);
+
+        btnDetail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent goToDetail = new Intent(DestinationActivity.this, DetailDestinationActivity.class);
+                startActivity(goToDetail);
+            }
+        });
 
         imbBack.setOnClickListener(new View.OnClickListener() {
             @Override
--- app/src/main/java/com/example/rajaampat/DetailDestinationActivity.java
+++ app/src/main/java/com/example/rajaampat/DetailDestinationActivity.java
@@ -3,18 +3,34 @@ package com.example.rajaampat;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class DetailDestinationActivity extends AppCompatActivity {
+
+    ImageButton btnBack;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail_destination);
 
-        ViewPager viewPager = findViewById(R.id.vpg_detail_destination);
-        ImageDetailActivityAdapter adapter = new ImageDetailActivityAdapter(this);
-        viewPager.setAdapter(adapter);
+//        ViewPager viewPager = findViewById(R.id.vpg_detail_destination);
+//        ImageDetailActivityAdapter adapter = new ImageDetailActivityAdapter(this);
+//        viewPager.setAdapter(adapter);
+
+        btnBack = findViewById(R.id.btn_back);
+
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent goToDestination = new Intent(DetailDestinationActivity.this, DestinationActivity.class);
+                startActivity(goToDestination);
+            }
+        });
 
     }
 }
