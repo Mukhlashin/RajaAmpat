923ce46189a6e2b7b52103a11321e2b1d9ec887c
Abdullah Mukhlashin <abdullahmukhlashiin@gmail.com>
2019-10-29 11:25:28 +0700

    Making Logout Feature but still in bug

--- app/src/main/java/com/example/rajaampat/MenuFragment.java
+++ app/src/main/java/com/example/rajaampat/MenuFragment.java
@@ -1,17 +1,22 @@
 package com.example.rajaampat;
 
 
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
-import java.util.zip.Inflater;
+import static android.content.Context.MODE_PRIVATE;
 
 
 /**
@@ -19,7 +24,9 @@ import java.util.zip.Inflater;
  */
 public class MenuFragment extends Fragment {
 
-    Button btnEditProfile, btnListPengaduan;
+    Button btnEditProfile, btnListPengaduan, btnLogout;
+
+    SharedPreferences.Editor editorLogin;
 
     public MenuFragment() {
         // Required empty public constructor
@@ -32,8 +39,11 @@ public class MenuFragment extends Fragment {
         // Inflate the layout for this fragment
         View layout = inflater.inflate(R.layout.fragment_menu, container, false);
 
+        editorLogin = this.getActivity().getSharedPreferences("login", MODE_PRIVATE).edit();
+
         btnEditProfile = layout.findViewById(R.id.btn_edit_profile);
         btnListPengaduan = layout.findViewById(R.id.btn_list_pengaduan);
+        btnLogout = layout.findViewById(R.id.btn_logout);
         btnListPengaduan.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -48,6 +58,40 @@ public class MenuFragment extends Fragment {
                 startActivity(intent);
             }
         });
+
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialogLogOut();
+            }
+        });
         return layout;
+    }
+
+    private void dialogLogOut() {
+        AlertDialog.Builder logOut = new AlertDialog.Builder(getActivity());
+        logOut.setTitle(R.string.logOut);
+        logOut.setMessage(R.string.messageLogout);
+        logOut.setPositiveButton("Logout", null);
+        logOut.setNegativeButton("Cancel", null);
+        logOut.setCancelable(false);
+        AlertDialog dialog = logOut.create();
+        logOut.show();
+
+        logOut.setPositiveButton(R.string.logOut, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                editorLogin.remove("login");
+                editorLogin.apply();
+
+                Intent logOut = new Intent(getActivity(), LoginActivity.class);
+                startActivity(logOut);
+                getActivity().finish();
+            }
+        });
+        logOut.setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+
+            }
+        });
     }
 }
